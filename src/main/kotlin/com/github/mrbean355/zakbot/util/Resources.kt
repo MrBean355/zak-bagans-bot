f79bdc700fdae07795c178677ae964b038e4e16f
Mike Johnston <mrbean355@gmail.com>
2021-04-11 22:26:32 +0200

    Fix resource loading in the JAR file

--- src/main/kotlin/com/github/mrbean355/zakbot/util/Resources.kt
+++ src/main/kotlin/com/github/mrbean355/zakbot/util/Resources.kt
@@ -1,14 +1,13 @@
 package com.github.mrbean355.zakbot.util
 
 import com.github.mrbean355.zakbot.phrases.Phrase
-import java.io.File
 
 fun readResourceFileLines(filePath: String): List<String> {
-    val url = Phrase::class.java.classLoader.getResource(filePath)
-    require(url != null) { "Failed to load resource: $filePath" }
+    val stream = Phrase::class.java.classLoader.getResourceAsStream(filePath)
+    require(stream != null) { "Failed to load resource: $filePath" }
 
-    return File(url.file).readLines().asSequence()
+    return stream.use { it.readBytes().decodeToString() }
+        .split("\n")
         .map { it.substringBefore('#').trim() }
         .filter { it.isNotEmpty() }
-        .toList()
 }
