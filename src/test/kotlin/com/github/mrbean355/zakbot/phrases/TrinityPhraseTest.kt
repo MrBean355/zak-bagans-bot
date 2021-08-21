3f63d2be5072cf3e3ac8144f3495cb7da92f55a3
Mike Johnston <mrbean355@gmail.com>
2021-08-21 11:48:14 +0200

    Add phrase for mocking the Trinity

---
+++ src/test/kotlin/com/github/mrbean355/zakbot/phrases/TrinityPhraseTest.kt
@@ -0,0 +1,17 @@
+package com.github.mrbean355.zakbot.phrases
+
+internal class TrinityPhraseTest : PhraseTest() {
+
+    override val creator = { TrinityPhrase() }
+
+    override val expectedPriority = 7
+
+    override val responseFileName = "phrases/trinity.txt"
+
+    override val replyChances = mapOf(
+        "hello world!" to 0f,
+        "I love the number 3!" to 0.5f,
+        "but not the number 33.." to 0f,
+        "Three is also pretty cool" to 0.5f,
+    )
+}
