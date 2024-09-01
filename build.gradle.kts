a6c0d7dc962ae2de754021325926f6504afa9b37
Mike Johnston <mrbean355@gmail.com>
2024-09-01 11:05:07 +0200

    Replace deprecated kotlinOptions usage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -17,8 +18,8 @@ java {
     targetCompatibility = JavaVersion.VERSION_17
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+kotlin {
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
 }
 
 tasks.getByName<Jar>("jar") {
