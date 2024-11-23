5a16ff9ce85621a457684ec1a65384dc03ba596b
Mike Johnston <mrbean355@gmail.com>
2024-11-23 17:06:39 +0200

    Fix markdown issues when handling commands
    Comment bodies will be encoded in Markdown, causing usernames like
    test_user to become test\_user, causing unexpected issues.
    Strip the Markdown syntax from comment bodies before processing
    commands.

--- src/test/kotlin/com/github/mrbean355/zakbot/util/MessagesKtTest.kt
+++ src/test/kotlin/com/github/mrbean355/zakbot/util/MessagesKtTest.kt
@@ -6,6 +6,18 @@ import org.junit.jupiter.api.Test
 internal class MessagesKtTest {
 
     @Test
+    fun testAsPlainText() {
+        assertEquals("\\", "\\".asPlainText())
+        assertEquals("*_*_*_", "\\*\\_\\*\\_\\*\\_".asPlainText())
+        assertEquals("Some bold text.", "Some **bold** text.".asPlainText())
+        assertEquals("Some italicised text.", "Some *italicised* text.".asPlainText())
+        assertEquals("Invalid `markdown", "Invalid `markdown".asPlainText())
+        assertEquals("test_username", "test\\_username".asPlainText())
+        assertEquals("!command ignore_user victim", "!command ignore\\_user victim".asPlainText())
+        assertEquals("Here are *multiple* _escapes_ in a comment.", "Here are \\*multiple\\* \\_escapes\\_ in a comment.".asPlainText())
+    }
+
+    @Test
     internal fun testCountOccurrences() {
         val input = "He found his art never progressed when he literally used his sweat and tears."
 
